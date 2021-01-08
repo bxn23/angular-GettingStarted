4540f5b35c1ea281d90a54382e38a7f712f5f611
bxn23 <bxn_23@hotmail.com>
2021-01-07 17:54:03 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductDetailsComponent } from "./product-details/product-details.compo
 import { CartService } from "./cart.service";
 import { CartComponent } from "./cart/cart.component";
 import { HttpClientModule } from "@angular/common/http";
-import { ShippingComponent } from './shipping/shipping.component';
+import { ShippingComponent } from "./shipping/shipping.component";
 
 @NgModule({
   imports: [
@@ -22,7 +22,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
       { path: "cart", component: CartComponent },
-      { path: "shipping", component: ShippingComponent },
+      { path: "shipping", component: ShippingComponent }
     ])
   ],
   declarations: [
